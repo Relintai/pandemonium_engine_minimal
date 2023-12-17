59a6cc90f4f347f53f98c03d22a1302f885e7bfd
Relintai <relintai@protonmail.com>
2023-12-17 20:02:51 +0100

    Removed freetype.

--- modules/freetype/uwpdef.h
+++
@@ -1,7 +0,0 @@
-
-/*  uwpdef.h                                                             */
-
-
-// "generic" is a reserved keyword in C++/CX code
-// this avoids the errors in the variable name from Freetype code
-#define generic freetype_generic
